f81bda46a19e35ee2ad67e80094720d359397f7f
Gopi Krishna M <gopt.muktevi@gmail.com>
2020-08-02 21:10:46 +0530

    Adding some state management for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+      path: '',
+      redirectTo: '/products',
+      pathMatch: 'full'
+  },
+  {
+      path: 'products',
+      component: ProductsComponent
+  },
+  {
+      path: '**',
+      redirectTo: '',
+      pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
