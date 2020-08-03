a537a18b9db788823ad7b0646a0bdc62dc019b36
Gopi Krishna M <gopt.muktevi@gmail.com>
2020-08-03 21:52:52 +0530

    Adding some state managment stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { StoreModule } from '@ngrx/store';
+import { productReducer } from './reducer/product/product.reducer';
 
 const routes: Routes = [
   {
@@ -17,6 +21,14 @@ const routes: Routes = [
       component: ProductsComponent
   },
   {
+      path: 'cart',
+      component: CartComponent
+  },
+  {
+      path: 'product/:id',
+      component: ProductComponent
+  },
+  {
       path: '**',
       redirectTo: '',
       pathMatch: 'full'
@@ -25,11 +37,14 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    StoreModule.forRoot({store: productReducer}),
     AppRoutingModule
   ],
   providers: [],
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from '../store/market';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+
+  products = PRODUCTS;
 
   constructor() { }
 
